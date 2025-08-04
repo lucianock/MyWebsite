ce67b49106e4d2d58054fa8bb93d0cd4daae2442
Luciano <lucianocampos.k@gmail.com>
2025-08-04 19:35:43 -0300

    Add static generation for blog post pages
    Implements getStaticPaths and getStaticProps to statically generate individual blog post pages based on slugs. Adds fallback loading state and passes blog data as props to the SingleBlog component.

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,11 +1,31 @@
 import SingleBlogDetails from "@/components/blog/SingleBlogDetails";
 import BreadcrumbHero from "@/components/shared/BreadcrumbHero";
 import Layout from "@/layout/Layout";
-import { singleBlogHero } from "@/staticData/blog/blog";
+import { singleBlogHero, allBlogs } from "@/staticData/blog/blog";
 import { useRouter } from "next/router";
 
-const SingleBlog = () => {
+const SingleBlog = ({ blog }) => {
   const router = useRouter();
+
+  // Si la página está cargando, mostrar un loading
+  if (router.isFallback) {
+    return (
+      <Layout>
+        <div id="blog">
+          <BreadcrumbHero heroData={singleBlogHero} />
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8">
+                <div className="text-center py-5">
+                  <h3>Cargando...</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
@@ -16,5 +36,34 @@ const SingleBlog = () => {
     </Layout>
   );
 };
+
+// Generar las rutas estáticas
+export async function getStaticPaths() {
+  const paths = allBlogs.map((blog) => ({
+    params: { slug: blog.slug },
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking', // Permite generar páginas dinámicamente en desarrollo
+  };
+}
+
+// Obtener los datos del blog
+export async function getStaticProps({ params }) {
+  const blog = allBlogs.find((blog) => blog.slug === params.slug);
+
+  if (!blog) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      blog,
+    },
+  };
+}
 
 export default SingleBlog;
